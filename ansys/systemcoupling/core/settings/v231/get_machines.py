bb8ae0bf9f5ffab133b0bd5682c4f7917199fd1e
Ian Boyd <93142264+iboyd-ansys@users.noreply.github.com>
2022-07-19 11:27:20 +0100

    Add missing newly generated files (#11)
    Add missing generated files (new)

---
+++ ansys/systemcoupling/core/settings/v231/get_machines.py
@@ -0,0 +1,13 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+from ansys.systemcoupling.core.settings.datamodel import *
+
+
+class get_machines(Command):
+    """
+    Get currently available machines.
+    """
+
+    syc_name = "GetMachines"
