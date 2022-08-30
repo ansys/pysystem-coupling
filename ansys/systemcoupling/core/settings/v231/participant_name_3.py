e1e0d98b0b77ca6e98ed3a119d1a2e62bfeb7ec8
Ian Boyd <ian.boyd@ansys.com>
2022-08-30 13:10:28 +0100

    missing files

---
+++ ansys/systemcoupling/core/settings/v231/participant_name_3.py
@@ -0,0 +1,13 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+from ansys.systemcoupling.core.settings.datamodel import *
+
+
+class participant_name(String):
+    """
+    Name of the participant
+    """
+
+    syc_name = "ParticipantName"
