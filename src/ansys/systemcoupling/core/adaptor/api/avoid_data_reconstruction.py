220d99c1134dee73f52be4835a619eda29bb5c4f
Ian Boyd <ian.boyd@ansys.com>
2022-09-15 11:49:13 +0100

    new generated file

---
+++ src/ansys/systemcoupling/core/adaptor/api/avoid_data_reconstruction.py
@@ -0,0 +1,36 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+from ansys.systemcoupling.core.adaptor.impl.datamodel import *
+
+
+class avoid_data_reconstruction(Group):
+    """
+    Control whether data reconstruction should be done for elemental intensive data.
+    """
+
+    syc_name = "AvoidDataReconstruction"
+
+    property_names_types = [
+        ("volume_mapping", "VolumeMapping", "Boolean"),
+        ("surface_mapping", "SurfaceMapping", "Boolean"),
+    ]
+
+    @property
+    def volume_mapping(self) -> Boolean:
+        """'volume_mapping' property of 'analysis_control' object"""
+        return self.get_property_state("volume_mapping")
+
+    @volume_mapping.setter
+    def volume_mapping(self, value: Boolean):
+        self.set_property_state("volume_mapping", value)
+
+    @property
+    def surface_mapping(self) -> Boolean:
+        """'surface_mapping' property of 'analysis_control' object"""
+        return self.get_property_state("surface_mapping")
+
+    @surface_mapping.setter
+    def surface_mapping(self, value: Boolean):
+        self.set_property_state("surface_mapping", value)
