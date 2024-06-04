3545d44713315c3850a342d797a2c2bbee283bc5
Marshall Hanmer <marshall.hanmer@ansys.com>
2024-06-04 12:14:37 -0400

    Add some debug echo statements

--- .ci/pull_syc_fluent_images.sh
+++ .ci/pull_syc_fluent_images.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Pulling pysystem-coupling Image Version ${SYC_IMAGE_VERSION}"
+echo "Pulling pyfluent Image Version ${FLUENT_IMAGE_VERSION}"
+
 docker pull ghcr.io/pyansys/pysystem-coupling:${SYC_IMAGE_VERSION} &
 docker pull ghcr.io/pyansys/pyfluent:${FLUENT_IMAGE_VERSION} &
 
