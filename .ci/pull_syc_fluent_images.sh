40402c12ae47f10f028043eb90263eaf5d4ab0e9
Marshall Hanmer <marshall.hanmer@ansys.com>
2024-06-04 12:28:11 -0400

    Fix a dumb mistake

--- .ci/pull_syc_fluent_images.sh
+++ .ci/pull_syc_fluent_images.sh
@@ -3,8 +3,8 @@
 echo "Pulling pysystem-coupling Image Version ${SYC_IMAGE_VERSION}"
 echo "Pulling pyfluent Image Version ${FLUENT_IMAGE_VERSION}"
 
-docker pull ghcr.io/pyansys/pysystem-coupling:${SYC_IMAGE_VERSION} &
-docker pull ghcr.io/pyansys/pyfluent:${FLUENT_IMAGE_VERSION} &
+docker pull ghcr.io/ansys/pysystem-coupling:${SYC_IMAGE_VERSION} &
+docker pull ghcr.io/ansys/pyfluent:${FLUENT_IMAGE_VERSION} &
 
 # Wait for the background processes to finish
 wait
