b53a70e03559927b5d30597017c19946a170c888
Ian Boyd <ian.boyd@ansys.com>
2024-07-22 11:20:12 +0100

    start filling out the oscillating plate documentation

--- examples/00-systemcoupling/oscillating_plate.py
+++ examples/00-systemcoupling/oscillating_plate.py
@@ -20,78 +20,117 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-""".. _ref_fsi_example:
+""".. _ref_oscillating_plate_example:
 
-fsi.py Example
--------------------------------------------------
+Oscillating plate
+-----------------
 
-This example demonstrates a simple fluid-structure interaction (FSI) simulation
+This example is a version of the *Oscillating Plate* case that is
+often used as a tutorial for System Coupling. This two-way, fluid-structural
+interaction (FSI) case is based on co-simulation of a transient oscillating
+plate with 2D data transfers.
+
+- Ansys Mechanical APDL (MAPDL) is used to perform a transient structural analysis.
+- Ansys Fluent is used to perform a transient fluid-flow analysis.
+- System Coupling coordinates the simultaneous execution of the solvers for
+  these Ansys products and the data transfers between their coupled surface regions.
+
+**Problem description**
+
+An oscillating plate resides within a fluid-filled cavity. A thin plate is
+anchored to the bottom of a closed cavity filled with fluid (air):
+
+.. image:: /_static/img_oscplate_case.png
+   :width: 400pt
+   :align: center
+
+There is no friction between the plate and the side of the cavity. An
+initial pressure of 100 Pa is applied to one side of the thin plate
+for 0.5 seconds to distort it. Once this pressure is released, the plate
+oscillates back and forth to regain its equilibrium, and the
+surrounding air damps this oscillation. The plate and surrounding
+air are simulated for a few oscillations to allow an examination of the
+motion of the plate as it is damped.
 
 """
+# %%
+# Set up example
+# --------------
+# Setting up this example consists of performing imports, downloading
+# input files, preparing the directory structure, and launching System Coupling.
+#
+# Perform required imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# Import ``ansys-systemcoupling-core``, ``ansys-fluent-core`` and
+# ``ansys-mapdl-core`` and other required packages.
 
-# import required modules
+# sphinx_gallery_thumbnail_path = '_static/oscplate_displacement.png'
 
 import ansys.fluent.core as pyfluent
 import ansys.mapdl.core as pymapdl
-from ansys.mapdl.core import LOG as MAPDL_LOG
 
 import ansys.systemcoupling.core as pysyc
 from ansys.systemcoupling.core import examples
 
-# Connect to mapdl container
-MAPDL_LOG.log_to_stdout("DEBUG")
-print("Connecting to MAPDL")
-# mapdl = pymapdl.Mapdl()
-mapdl = pymapdl.launch_mapdl(version=242)
-# ================================
-
-# read in the pre-created Fluent case file and launch Fluent
-
-# print(f"Sleeping - PID = {os.getpid()}")
-# time.sleep(30)
+# %%
+#
+# Download input file
+# ~~~~~~~~~~~~~~~~~~~
+# This example uses one pre-created file - a Fluent input file that contains
+# the fluids setup.
+#
 fluent_cas_file = examples.download_file(
     "plate.cas.gz", "pysystem-coupling/oscillating_plate/Fluent"
 )
 
-# shutil.copy(fluent_cas_file, "plate.cas.gz")
-
-# fluent_cas_file = "plate.cas.gz"
-
-print("Launching Fluent Container")
-fluent = pyfluent.launch_fluent(start_transcript=True, product_version="242")
-# custom_config = {"fluent_image": "ghcr.io/ansys/pyfluent:v24.2.0"}
-# fluent = pyfluent.launch_fluent(start_transcript=True, container_dict=custom_config)
-fluent.file.read(file_type="case", file_name=fluent_cas_file)
-
-# ================================
-
-# launch System Coupling
-print("Launching System Coupling Container")
+# %%
+# Launch and connect to System Coupling
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Launch a remote System Coupling instance and return a *client* object
+# (a ``Session`` object) that allows you to interact with System Coupling
+# via an API exposed into the current Python environment.
 syc = pysyc.launch()
 
-# ================================
 
+# %%
+# Launch and connect to Fluent
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Use the created ``fluent`` session object to read the case file.
+fluent = pyfluent.launch_fluent(start_transcript=True, product_version="242")
+fluent.file.read(file_type="case", file_name=fluent_cas_file)
+
+# %%
+# Launch and connect to MAPDL
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Use the ``mapdl`` session object to set up the structural side of the FSI
+# case.
+mapdl = pymapdl.launch_mapdl(version=242)
 mapdl.prep7()
 
-# define material properties
+# %%
+# Define material properties.
 mapdl.mp("DENS", 1, 2550)  # density
 mapdl.mp("ALPX", 1, 1.2e-05)  # thermal expansion coefficient
 mapdl.mp("EX", 1, 2500000)  # Young's modulus
 mapdl.mp("NUXY", 1, 0.35)  # Poisson's ratio
 
-# set element types to SOLID186
+# %%
+# Set element types to SOLID186.
 mapdl.et(1, 186)
 mapdl.keyopt(1, 2, 1)
 
-# make geometry
+# %%
+# Make geometry.
 mapdl.block(10.00, 10.06, 0.0, 1.0, 0.0, 0.4)
 mapdl.vsweep(1)
 
-# add fixed support at y=0
+# %%
+# Add fixed support at y=0.
 mapdl.run("NSEL,S,LOC,Y,0")
 mapdl.d("all", "all")
 
-# add FSI interface
+# %%
+# Add the FSI interface.
 mapdl.run("NSEL,S,LOC,X,9.99,10.01")
 mapdl.run("NSEL,A,LOC,Y,0.99,1.01")
 mapdl.run("NSEL,A,LOC,X,10.05,10.07")
@@ -102,7 +141,8 @@ mapdl.allsel()
 
 mapdl.run("/SOLU")
 
-# set analysis type to steady
+# %%
+# Set analysis type to steady.
 mapdl.antype(4)
 
 mapdl.nlgeom("ON")  # large deformations
@@ -114,15 +154,26 @@ mapdl.run("nsub,1,1,1")
 mapdl.run("time,10.0")
 mapdl.timint("on")
 
-# ================================
-
-# syc.start_output()
+# %%
+# Create the coupled analysis
+# ---------------------------
+# Creating the analysis consists of accessing the ``setup`` API,
+# adding the participants, creating and verifying both interfaces and
+# data transfers, querying for setup errors, and modifying settings.
+#
+# Access the ``setup`` API
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+setup = syc.setup
 
 syc.setup.activate_hidden.beta_features = True
 syc.setup.activate_hidden.alpha_features = True
 syc.setup.activate_hidden.lenient_validation = True
 
-# add participants
+# %%
+# Add participants
+# ~~~~~~~~~~~~~~~~
+# Add the ``fluent`` and ``mapdl`` sessions as participants in the
+# analysis.
 fluid_name = syc.setup.add_participant(participant_session=fluent)
 solid_name = syc.setup.add_participant(participant_session=mapdl)
 
