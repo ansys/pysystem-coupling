9ce3a9beee82c091b0d86a454de5e629ee5047d6
Ian Boyd <ian.boyd@ansys.com>
2025-10-08 12:51:48 +0100

    add 'make level map' test

--- tests/test_named_object_level_map.py
+++ tests/test_named_object_level_map.py
@@ -19,9 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-# import pytest
-
 from ansys.systemcoupling.core.adaptor.impl.static_info import (
     make_named_object_level_map,
 )
