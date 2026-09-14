8c5aaee3009f7ed3316f6d429a026cd88a8cdd71
Ian Boyd <ian.boyd@ansys.com>
2026-09-14 14:08:14 +0100

    tweak debug print in example

--- examples/00-systemcoupling/cht_pipe.py
+++ examples/00-systemcoupling/cht_pipe.py
@@ -311,7 +311,11 @@ syc.setup.solution_control.end_time = 10  # end time is 10.0 [s]
 syc.setup.output_control.option = "EveryStep"
 syc.setup.output_control.generate_csv_chart_output = True
 
-syc.setup.print_state()  ### !!!TEMPORARY!!! ###
+from ansys.systemcoupling.core import LOG
+
+LOG.info(
+    f"Printing system coupling setup state...\n>>>\n{syc.setup.print_state()}\n<<<\n"
+)  ### !!!TEMPORARY!!! ###
 
 # %%
 # Solution
